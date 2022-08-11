0ac9405cd8f87928e85ce2930b322b7559acc19b
jevazquezb <je.vazquezb@gmail.com>
2022-08-10 23:05:54 -0500

    Adjust indentation in data.sql and schema.sql files

--- schema.sql
+++ schema.sql
@@ -11,12 +11,25 @@ CREATE TABLE animals (
   PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD COLUMN species VARCHAR(100);
-ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD COLUMN species_id INT;
-ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
-ALTER TABLE animals ADD COLUMN owner_id INT;
-ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE;
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(100);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id)
+ON DELETE CASCADE;
 
 /* owners table */
 CREATE TABLE owners (

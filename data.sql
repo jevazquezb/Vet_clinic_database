ee9cb6aca207ab5eaeff74a05e3bfa29ce3d0ad4
jevazquezb <je.vazquezb@gmail.com>
2022-08-10 19:06:20 -0500

    Insert data into owners and species tables, and set values for species_id and owner_id columns from the animals' table

--- data.sql
+++ data.sql
@@ -1,9 +1,11 @@
 /* Populate database with sample data. */
 
+/* animals' table */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, TRUE, 8.0);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-05-12', 5, TRUE, 11.0);
+
 INSERT INTO
   animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
@@ -14,3 +16,62 @@ VALUES
   ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
   ('Blossom', '1998-10-13', 3, TRUE, 17.0),
   ('Ditto', '2022-05-14', 4, TRUE, 22.0);
+
+/* owners' table */
+INSERT INTO
+  owners (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+/* species' table */
+INSERT INTO
+  species (name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+/* Updating animals' table */
+UPDATE animals
+SET species_id = species.id
+FROM species
+WHERE animals.name LIKE '%mon%' AND species.name = 'Digimon';
+
+UPDATE animals
+SET species_id = species.id
+FROM species
+WHERE species_id IS NULL AND species.name = 'Pokemon';
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name IN ('Agumon') AND owners.full_name = 'Sam Smith'
+RETURNING *;
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name IN ('Gabumon', 'Pikachu') AND owners.full_name = 'Jennifer Orwell'
+RETURNING *;
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name IN ('Devimon', 'Plantmon') AND owners.full_name = 'Bob'
+RETURNING *;
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name IN ('Charmander', 'Squirtle', 'Blossom') AND owners.full_name = 'Melody Pond'
+RETURNING *;
+
+UPDATE animals
+SET owner_id = owners.id
+FROM owners
+WHERE animals.name IN ('Angemon', 'Boarmon') AND owners.full_name = 'Dean Winchester'
+RETURNING *;
